14f03fa6820846d8fb51459f824f5792f8f874e0
mostafa <abwmrwf434@gmail.com>
2025-01-28 18:50:31 +0200

    quran page is complete

--- src/app/pages/quran/quran.component.ts
+++ src/app/pages/quran/quran.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';  // Import here
+
+
 
 @Component({
   selector: 'app-quran',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './quran.component.html',
-  styleUrl: './quran.component.css'
+  styleUrl: './quran.component.css',
 })
-export class QuranComponent {
+
+
+export class QuranComponent implements OnInit {
+  surahs: any[] = []; // Store the data here
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://api.alquran.cloud/v1/surah') // API URL
+      .subscribe((response: any) => {
+        this.surahs = response.data; // Get the "data" part from the response
+        console.log(this.surahs); // Check the data in the console
+      });
+  }
 
 }
+
